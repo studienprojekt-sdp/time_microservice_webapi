68ffed68808c72e094ba98b8c9806d5b5bf7ec94
jnhck <s2jaheuc@uni-trier.de>
2021-10-03 00:39:32 +0200

    kinda works

--- zeitservice.js
+++ zeitservice.js
@@ -1,5 +1,5 @@
 const axios = require("axios"); // using axios to handle the http request
-const express = require("express"); 
+const express = require("express"); // express for building the web app
 const app = express();
 
 const port = 3000;
@@ -8,44 +8,26 @@ app.listen(port, () => {
   console.log("Listening at http://localhost:" + port);
 });
 
-app.get("/api/timezone/:location", function(req, res) {
-  res.send(req.params.location); // response is sent here
+app.get("/api/timezone/tz=:timezone(*)", function(req, res) { // the (*) is important, otherwise express will think "/" in the timezone (e.g. in Europe/Berlin) is another route
+  const timezone = req.params.timezone; // the timezone that is passed via the URL
+  const requestURL = "https://api.ipgeolocation.io/timezone?apiKey=59149b3287fd4d55b0d992b9cf2924ea&tz=" + timezone;
+
+  var config = {
+    method: 'get',
+    url: requestURL,
+    headers: {}
+  };
+  
+  axios(config)
+  .then(function (response) {
+    res.send(response.data); // we just forward the entire .json
+  })
+  .catch(function (error) {
+    if (error.response) {
+      res.status(404).send("Error occurred! Reason: " + error.response.data)
+    }
+    else {
+      res.status(404).send("Unknown error occurred!")
+    }
+  });
 });
-
-/*
-
-let requestURL;
-let localTime = true;
-
-// the process.argv[] array saves the parameters, if [2] is null, we return the time at the current ip address
-// if [2] is not null we return the time for the specified timezone
-if (process.argv[2]  != null) {
-    requestURL = "https://api.ipgeolocation.io/timezone?apiKey=59149b3287fd4d55b0d992b9cf2924ea&tz=" + process.argv[2];
-    localTime = false;
-} else {
-    requestURL = "https://api.ipgeolocation.io/timezone?apiKey=59149b3287fd4d55b0d992b9cf2924ea";
-}
-
-var config = {
-  method: 'get',
-  url: requestURL,
-  headers: {}
-};
-
-axios(config)
-.then(function (response) {
-  console.log(response.data.date_time_ymd);
-})
-.catch(function (error) {
-  if (localTime) {
-      console.log(new Date());
-  }
-  else if (error.response) {
-      console.log("Error occurred! Reason: " + error.response.data.message);
-  }
-  else {
-    console.log("Unknown error occurred!")
-  }
-});
-
-*/
