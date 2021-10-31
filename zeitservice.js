5aa7180e5282542a0be4fc8d50b47806f4b29abd
jnhck <s2jaheuc@uni-trier.de>
2021-10-31 13:50:53 +0100

    no frontend here

--- zeitservice.js
+++ zeitservice.js
@@ -1,18 +1,11 @@
 const axios = require("axios"); // using axios to handle the http request
 const express = require("express"); // express for building the web app
 const app = express();
-const path = require('path');
-
-app.use(express.static("public"));
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log("Listening at port:" + port);
-});
-
-app.get("*", function(req, res) {
-  res.sendFile("index.html");
 });
 
 app.get("/api/timezone/tz=:timezone(*)", function(req, res) { // the (*) is important, otherwise express will think "/" in the timezone (e.g. in Europe/Berlin) is another route
