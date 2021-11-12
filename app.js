3d513aeb45619945cfb3afa3e0a00d3548542ae9
jnhck <s2jaheuc@uni-trier.de>
2021-11-12 13:54:56 +0100

    prep work

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const axios = require("axios"); 
-const express = require("express"); 
-const app = express();
+import express from "express";
+import axios from "axios";
 
+const app = express();
 const port = 4000;
 
 app.listen(port, () => {
@@ -12,7 +12,7 @@ app.get("/api/timezone/", function(req, res) {
   const timezone = req.query.tz;
   const requestURL = "https://api.ipgeolocation.io/timezone?apiKey=59149b3287fd4d55b0d992b9cf2924ea&tz=" + timezone;
 
-  var config = {
+  const config = {
     method: 'get',
     url: requestURL,
     headers: {}
@@ -20,7 +20,7 @@ app.get("/api/timezone/", function(req, res) {
   
   axios(config)
   .then(function (response) {
-    res.send(response.data); // we just forward the entire .json
+    res.status(200).send(response.data); // we just forward the entire .json
   })
   .catch(function (error) {
     if (error.response) {
